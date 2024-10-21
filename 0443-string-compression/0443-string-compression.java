94eb5f7e2912445f780d066b477ab56185075cf5
divya krishnamoorthi <162394345+Divya20085@users.noreply.github.com>
2024-10-21 14:13:45 +0530

    Time: 1 ms (99.47%), Space: 44.5 MB (12.87%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,28 +1,58 @@
+// class Solution {
+//     public int compress(char[] chars) 
+//     {
+//         int n= chars.length;
+//         StringBuilder sb = new StringBuilder();
+//         int f=1;
+//         for(int i=0;i<n;i++)
+//         {
+//             char c =chars[i];
+//             while( i<n-1&&chars[i]==chars[i+1])
+//             {
+//                 f++;
+//                 i++;
+//             }
+//             sb.append(c);
+//             if(f>1)
+//             sb.append(f);
+//             f=1;
+//         }
+//         String s1=sb.toString();
+//         char arr[]=s1.toCharArray();
+//         for(int i=0;i<arr.length;i++)
+//         {
+//             chars[i]=arr[i];
+//         }
+//         return arr.length;
+//     }
+// }
 class Solution {
     public int compress(char[] chars) 
     {
-        int n= chars.length;
-        StringBuilder sb = new StringBuilder();
-        int f=1;
-        for(int i=0;i<n;i++)
+       StringBuilder sb= new StringBuilder();
+       int n=chars.length;
+       int f=1;
+       for(int i=0;i<n;i++)
+       {
+        char c=chars[i];
+        while(i<n-1&&chars[i]==chars[i+1])
         {
-            char c =chars[i];
-            while( i<n-1&&chars[i]==chars[i+1])
-            {
-                f++;
-                i++;
-            }
-            sb.append(c);
-            if(f>1)
+            f++;
+            i++;
+        }
+        sb.append(c);
+        if(f>1)
+        {
             sb.append(f);
-            f=1;
         }
-        String s1=sb.toString();
-        char arr[]=s1.toCharArray();
-        for(int i=0;i<arr.length;i++)
-        {
-            chars[i]=arr[i];
-        }
-        return arr.length;
+        f=1;
+       }
+       String s=sb.toString();
+       char ch[]=s.toCharArray();
+       for(int i=0;i<ch.length;i++)
+       {
+        chars[i]=ch[i];
+       }
+       return ch.length;
     }
 }
