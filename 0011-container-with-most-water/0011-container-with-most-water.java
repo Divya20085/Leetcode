08f69d632e498e2e5fc7503888d16fb093b27de9
divya krishnamoorthi <162394345+Divya20085@users.noreply.github.com>
2025-01-04 14:08:54 +0530

    Time: 5 ms (74.5%), Space: 58 MB (39.81%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,26 +1,51 @@
 class Solution {
-    public int maxArea(int[] height) {
-        int left = 0;
-        int right = height.length - 1;
-        int maxArea = 0;
-
-        while(left<right)
+    public int maxArea(int[] height) 
+    {
+        int l=0;
+        int r=height.length-1;
+        int maxarea=0;
+        while(l<r)
         {
-            int width = right-left;
-            int currentHeight = Math.min(height[left] , height[right]);
-            int area = currentHeight * width;
-            maxArea = Math.max(area , maxArea);
-            if(height[left]  < height[right])
+            int w=r-l;
+            int h=Math.min(height[l],height[r]);
+            int area = h*w;
+            maxarea = Math.max(area, maxarea);
+            if(height[l]<height[r])
             {
-                left++; 
+                l++;
             }
             else
             {
-                right--;
+                r--;
             }
-            
         }
-        return maxArea;
+        return maxarea;
 
     }
 }
+// class Solution {
+//     public int maxArea(int[] height) {
+//         int left = 0;
+//         int right = height.length - 1;
+//         int maxArea = 0;
+
+//         while(left<right)
+//         {
+//             int width = right-left;
+//             int currentHeight = Math.min(height[left] , height[right]);
+//             int area = currentHeight * width;
+//             maxArea = Math.max(area , maxArea);
+//             if(height[left]  < height[right])
+//             {
+//                 left++; 
+//             }
+//             else
+//             {
+//                 right--;
+//             }
+            
+//         }
+//         return maxArea;
+
+//     }
+// }
